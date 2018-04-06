84c6c7adadf9f48d93d0236f749e5196d20c9df0
Toshiya Nakakura <nakakura@me.com>
2018-04-06 16:10:16 +0900

    first commit

---
+++ answer/js/main.js
@@ -0,0 +1,97 @@
+'use strict';
+
+let feedOfferButton;
+let feedOfferTextArea;
+let answerTextArea;
+
+let iceTextArea;
+let feedIceButton;
+let feedIceTextArea;
+
+let startButton;
+let callButton;
+let remoteVideo;
+
+let localStream;
+let pc;
+
+window.onload = function() {
+    feedOfferButton = document.getElementById('feed_offer');
+    feedOfferTextArea = document.getElementById('feed_offer_textarea');
+    feedOfferButton.onclick = feedOffer;
+
+    answerTextArea = document.getElementById('answer_textarea');
+
+    iceTextArea = document.getElementById('local_ice');
+    feedIceTextArea = document.getElementById('feed_ice');
+    feedIceButton = document.getElementById('feed_ice_button');
+    feedIceButton.onclick = feedIce;
+
+    remoteVideo = document.getElementById('remoteVideo');
+};
+
+function gotStream(stream) {
+  localStream = stream;
+  callButton.disabled = false;
+}
+
+function start() {
+  startButton.disabled = true;
+  navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true
+  })
+  .then(gotStream)
+  .catch(function(e) {
+    alert('getUserMedia() error: ' + e.name);
+  });
+}
+
+function feedOffer() {
+  let text = feedOfferTextArea.value;
+  let desc = JSON.parse(text);
+
+  let servers = null;
+  pc = new RTCPeerConnection(servers);
+  pc.onicecandidate = function(e) {
+    onIceCandidate(pc, e);
+  };
+  pc.ontrack = gotRemoteStream;
+
+  pc.setRemoteDescription(desc).then(
+      function() {},
+      function (err) {}
+  );
+  // Since the 'remote' side has no media stream we need
+  // to pass in the right constraints in order for it to
+  // accept the incoming offer of audio and video.
+  pc.createAnswer().then(
+      onCreateAnswerSuccess,
+      function (err) {}
+  );
+}
+
+function gotRemoteStream(e) {
+  if (remoteVideo.srcObject !== e.streams[0]) {
+    remoteVideo.srcObject = e.streams[0];
+  }
+}
+
+function onCreateAnswerSuccess(desc) {
+  answerTextArea.innerText = JSON.stringify(desc);
+
+  pc.setLocalDescription(desc).then(
+      function() {},
+      function (err) {}
+  );
+}
+
+function onIceCandidate(pc, event) {
+  iceTextArea.innerText = iceTextArea.value + "\n" + JSON.stringify(event.candidate);
+}
+
+function feedIce() {
+    let candidate = JSON.parse(feedIceTextArea.value);
+    pc.addIceCandidate(candidate);
+}
+
---
+++ offer/js/main.js
@@ -0,0 +1,130 @@
+'use strict';
+
+let createOfferButton;
+let offerTextArea;
+
+let feedAnswerButton;
+let feedAnswerTextArea;
+
+let iceTextArea;
+let feedIceTextArea;
+let feedIceButton;
+
+let startButton;
+let callButton;
+
+let startTime;
+let localVideo;
+
+let localStream;
+let pc;
+let offerOptions = {
+    offerToReceiveAudio: 1,
+    offerToReceiveVideo: 1
+};
+
+window.onload = function() {
+    createOfferButton = document.getElementById('create_offer');
+    offerTextArea = document.getElementById('offer_textarea');
+
+    feedAnswerButton = document.getElementById('feed_answer');
+    feedAnswerTextArea = document.getElementById('feed_answer_textarea');
+    feedAnswerButton.onclick = feedAnswer;
+
+    iceTextArea = document.getElementById('local_ice');
+    feedIceTextArea = document.getElementById('feed_ice');
+    feedIceButton = document.getElementById('feed_ice_button');
+    feedIceButton.onclick = feedIce;
+
+    startButton = document.getElementById('startButton');
+    callButton = document.getElementById('callButton');
+    callButton.disabled = true;
+    startButton.onclick = start;
+    callButton.onclick = call;
+
+    localVideo = document.getElementById('localVideo');
+    localVideo.addEventListener('loadedmetadata', function () {
+    });
+};
+
+function gotStream(stream) {
+    localVideo.srcObject = stream;
+    localStream = stream;
+    callButton.disabled = false;
+}
+
+function start() {
+    startButton.disabled = true;
+    navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true
+    })
+        .then(gotStream)
+        .catch(function(e) {
+            alert('getUserMedia() error: ' + e.name);
+        });
+}
+
+function call() {
+    callButton.disabled = true;
+    startTime = window.performance.now();
+    let videoTracks = localStream.getVideoTracks();
+    let audioTracks = localStream.getAudioTracks();
+    if (videoTracks.length > 0) {
+    }
+    if (audioTracks.length > 0) {
+    }
+    let servers = null;
+    pc = new RTCPeerConnection(servers);
+    pc.onicecandidate = function(e) {
+        onIceCandidate(pc, e);
+    };
+
+    localStream.getTracks().forEach(
+        function(track) {
+            pc.addTrack(
+                track,
+                localStream
+            );
+        }
+    );
+
+    pc.createOffer(
+        offerOptions
+    ).then(
+        onCreateOfferSuccess,
+        function (err) {});
+}
+
+function onCreateOfferSuccess(desc) {
+    offerTextArea.innerText = JSON.stringify(desc);
+
+    pc.setLocalDescription(desc).then(
+        function() {},
+        function (err) { }
+    );
+}
+
+function feedAnswer() {
+    let text = feedAnswerTextArea.value;
+    let desc = JSON.parse(text);
+
+    pc.setRemoteDescription(desc).then(
+        function () {},
+        function (err) {}
+    );
+}
+
+function feedIce() {
+    let candidate = JSON.parse(feedIceTextArea.value);
+    pc.addIceCandidate(candidate)
+        .then(
+            function() {},
+            function(err) {}
+        );
+}
+
+function onIceCandidate(pc, event) {
+    iceTextArea.innerText = iceTextArea.value + "\n" + JSON.stringify(event.candidate);
+}
+
